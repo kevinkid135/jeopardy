0add76a9f5a406184befb2d683d4ed95a64f39d6
Kevin Chan <kevinkid228@hotmail.com>
2019-03-27 02:41:17 -0400

    swapped table to use divs and filled in template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-$("td").hover(
+$(".itemOption").hover(
     function(){$(this).stop().animate({"opacity": "0.5"}, "fast");},
     function(){$(this).stop().animate({"opacity": "1"}, "fast");}
 );
-$("td").click(function(){
-    $(this).empty();
+
+$(".points").click(function(){
+    $(this).fadeOut(300, () => {this.remove();})
 });
+$(".question").click(function(){
+    $(this).fadeOut(300, () => {this.remove();})
+});
+$(".answer").click(function(){
+    $(this).fadeOut(300, () => {this.remove();})
+});
+// $(".divText").click(function(){
+//     $(this).fadeOut(300, () => {this.remove();})
+// });
